2ef40a5abb0fd6c350892c2e4ae8edb28d12e615
tmhuyett <toddhuyett@gmail.com>
2021-10-17 16:14:28 -0400

    'd'

--- starter/starter/catpoint-parent/securityService/src/main/java/module-info.java
+++ starter/starter/catpoint-parent/securityService/src/main/java/module-info.java
@@ -1,2 +1,13 @@
-module securityService {
+module com.udacity.catpoint.securityService {
+
+
+    requires com.udacity.catpoint.imageService;
+    requires miglayout;
+    requires java.desktop;
+    requires java.prefs;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.sql;
+
+    exports com.udacity.catpoint.security.service;
 }
